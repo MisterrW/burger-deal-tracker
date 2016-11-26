1c2e084c3dd06be06fd108ea5d4df417e7089cfb
William Robertson <w.robertson305@gmail.com>
2016-11-26 19:04:40 +0000

    plan nested hash map transform for deals view

---
+++ scratch/hash_practice.rb
@@ -0,0 +1,23 @@
+
+
+Eateries = {
+  'Bobbys' => {
+    'Bobbys budget bonanza' => {
+      'burgers_included' => ['Bobbys Basic Beef'],
+      'deal_type' => 'burger and a pint',
+      'day' => 'Wednesday'
+      },
+    'Bobbys Half-off Mondays' => {
+      'burgers_included' => ['Bobbys Basic Beef'],
+      'deal_type' => 'half price',
+      'day' => 'Monday'
+    },
+  },
+  'Hanks' => {
+    'Hanks Half-Price Happiness' => {
+      'burgers_included' => ['Hanks heap of hog', 'Hanks Vegan Surprise'],
+      'deal_type' => 'half price',
+      'day' => 'Friday'
+    },
+  }
+}
