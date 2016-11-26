3a96642ea327a2b34827d2ff8df9aa7b160cc68a
William Robertson <w.robertson305@gmail.com>
2016-11-26 19:45:32 +0000

    all by deal hash map for view almost working

--- console.rb
+++ console.rb
@@ -9,5 +9,21 @@ require_relative('db/seeds')
 
 Seeds.setup
 
+all_combos = Deal.all_pretty
+all_by_eatery = all_combos.group_by { |x| x['eatery']}
+# all_by_deal = all_by_eatery.each do |x|
+#   binding.pry
+#   x.group_by { |y| y['deal']}
+# end
+
+
+all_by_deal = {}
+all_by_eatery.each do |key,value|
+  temp_thing = {key => value.group_by {|x| x['deal']}}
+  # binding.pry
+  all_by_deal.merge!(temp_thing)
+end
+
+
 binding.pry
 nil
